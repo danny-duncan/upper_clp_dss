05486ffa4daadb19e5ceaf31898338a1b9fca385
SamStruthers <sam.struthers2000@gmail.com>
2025-10-31 09:50:59 -0600

    removing ross theme from script

--- src/setup_libraries.R
+++ src/setup_libraries.R
@@ -130,29 +130,5 @@ sensor_meta <- tibble(
 
 #Seasonal Color values
 #season_color_vals =c('#047E82','#397534','#59B851','#DEB907','#FA850E')
-require(tidyverse)
-require(ggthemes)
-# basic theme for all ggplots, if Roboto is not installed, just use default, but message
-if ({
-  require(systemfonts)
-  ("Roboto" %in% system_fonts()$family)
-}) {
-  ROSS_theme <- theme_bw() + #or theme_few()
-    theme(plot.title = element_text(hjust = 0.5, face = 'bold', family = "Roboto"),
-          plot.subtitle = element_text(hjust = 0.5, family = "Roboto"))
-} else {
-  message("You do not have the Roboto font family installed on your computer, currenly using ggplot default text family.
-          See ROSS_themes.R for directions to install the font family on your computer.")
-  ROSS_theme <- theme_bw() + #or theme_few()
-    theme(plot.title = element_text(hjust = 0.5, face = 'bold'),
-          plot.subtitle = element_text(hjust = 0.5))
-}
 
-ROSS_lt_pal <- c("#002EA3", "#E70870", "#256BF5",
-                 "#745CFB", "#1E4D2B", "#56104E")
-
-ROSS_dk_pal <- c("#1E4D2B", "#E70870", "#256BF5",
-                 "#FFCA3A", "#745CFB", "#C3F73A")
-
-ROSS_acc_pal <- c("#56104E", "#745CFB", "#FFCA3A", "#1E4D2B")
 
